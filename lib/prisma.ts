1f7783c6ca642e27f104d10685e8085c2a1cac15
Purnendu-Touchcore <purnendum@touchcoresystems.com>
2025-03-03 02:46:33 +0530

    miafd

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,4 @@
 // @ts-nocheck
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-    const prismaClient: PrismaClient | undefined
-}
-
-const prismaClient: PrismaClient = global.prismaClient || new PrismaClient();
-
-if (process.env.NODE_ENV === 'production') {
-    prismaClient = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prismaClient = global.prisma;
-}
-
-export const prismaClient;
+export const prismaClient = new PrismaClient();
