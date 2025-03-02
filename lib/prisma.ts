ff316abd332ee79409113ea71f9fbc8fea78f1b2
Purnendu-Touchcore <purnendum@touchcoresystems.com>
2025-03-03 02:31:46 +0530

    minorchange

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,15 @@
-import { PrismaClient } from "@prisma/client";
-// convert this to a singleton for nextjs
-export const prismaClient = new PrismaClient();
+// @ts-nocheck
+import { PrismaClient } from '@prisma/client';
+
+const prismaClient: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+    prismaClient = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prismaClient = global.prisma;
+}
+
+export const prismaClient;
