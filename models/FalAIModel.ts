c4ba74e01be47166dbc9f80f02b5cc161d110bb0
Purnendu-Touchcore <purnendum@touchcoresystems.com>
2025-03-04 11:06:18 +0530

    trainModel function updated in FalAIModel.ts

--- models/FalAIModel.ts
+++ models/FalAIModel.ts
@@ -1,5 +1,6 @@
 import { fal } from "@fal-ai/client";
 import { BaseModel } from "./BaseModel";
+const axios = require('axios');
 
 export class FalAIModel {
   constructor() {
@@ -20,13 +21,41 @@ export class FalAIModel {
 
   public async trainModel(zipUrl: string, triggerWord: string) {
     
-    const { request_id, response_url } = await fal.queue.submit("fal-ai/flux-lora-fast-training", {
-        input: {
-            images_data_url: zipUrl,
-            trigger_word: triggerWord
+    // const { request_id, response_url } = await fal.queue.submit("fal-ai/flux-lora-fast-training", {
+    //     input: {
+    //         images_data_url: zipUrl,
+    //         trigger_word: triggerWord
+    //     },
+    //     webhookUrl: `${process.env.WEBHOOK_BASE_URL}/fal-ai/webhook/train`,
+    // });
+
+    // curl --request POST \
+    // --url https://queue.fal.run/fal-ai/flux-lora-fast-training?fal_webhook=${process.env.WEBHOOK_BASE_URL}/fal-ai/webhook/train' \
+    // --header "Authorization: Key $FAL_KEY" \
+    // --header "Content-Type: application/json" \
+    // --data '{
+    //    "images_data_url": ""
+    //    "trigger_word": triggerWord
+    //  }'
+
+    const response = await axios.post(
+      'https://queue.fal.run/fal-ai/flux-lora-fast-training',
+      {
+        images_data_url: zipUrl,
+        trigger_word: triggerWord
+      },
+      {
+        headers: {
+          'Authorization': `Key ${process.env.FAL_KEY}`,
+          'Content-Type': 'application/json'
         },
-        webhookUrl: `${process.env.WEBHOOK_BASE_URL}/fal-ai/webhook/train`,
-    });
+        params: {
+          fal_webhook: `${process.env.WEBHOOK_BASE_URL}/fal-ai/webhook/train`
+        }
+      }
+    );
+
+    const { request_id, response_url } = response.data;
 
     return { request_id, response_url };
   }
